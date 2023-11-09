b172d73a4530072cd8dccc45728866bb307acb57
Morgan Laymon <LAYMONM@ETSU.EDU>
2023-11-08 19:43:20 -0500

    Initialized and Organized several starter files and directories.

--- FragmentsOfMythos.cs
+++ FragmentsOfMythos.cs
@@ -1,8 +1,16 @@
+using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Input;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Generation;
 
 namespace FragmentsOfMythos
 {
 	public class FragmentsOfMythos : Mod
 	{
+		// Test
 	}
+
+
 }
